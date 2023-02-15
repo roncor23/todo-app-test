4800da95d9bd38e77a68db84df3728c32b8ce856
roncor23 <ronanotaza@gmail.com>
2023-02-15 13:07:48 +0800

    todo app test

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app class="main">
+    <v-main class="py-15">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+<style scoped>
+  .main {
+    background: #fff;
+  }
+</style>
